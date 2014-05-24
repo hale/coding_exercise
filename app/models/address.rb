4c0d8db187b1d83902969051c2cf043befbf8772
Philip Hale <philip@pghale.com>
2014-05-24 13:42:02 +0100

    Street address validations.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,4 @@
 class Address < ActiveRecord::Base
+  validates_format_of :line_1, with: /\A[\p{L}\d ]*\z/
+  validates_format_of :line_2, with: /\A[\p{L}\d ]*\z/
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Address, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    [:line_1, :line_2].each do |street_addr|
+      it { should allow_value("34 Jute Street").for(street_addr) }
+      it { should allow_value("23 Jétçe タカ").for(street_addr) }
+
+      it { should_not allow_value("!").for(street_addr) }
+      it { should_not allow_value("23\nsomething").for(street_addr) }
+      it { should_not allow_value("street!").for(street_addr) }
+    end
+
+  end
 end
