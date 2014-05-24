806272f853df5c83d97acdebef7848e3e2a6c73d
Philip Hale <philip@pghale.com>
2014-05-24 14:59:01 +0100

    Validations for first and last name.
    Any alphabetical characters allowed
    Both first and last name must be present.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ActiveRecord::Base
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
+  validates_format_of :first_name, with: /\A[\p{L}]*\z/
+  validates_format_of :last_name, with: /\A[\p{L}]*\z/
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 describe Contact, :type => :model do
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+
+  [:first_name, :last_name].each do |name|
+    it { should allow_value("John").for(name) }
+    it { should allow_value("amy").for(name) }
+    it { should allow_value("بالعربي").for(name) }
+
+    it { should_not allow_value("G4m3r").for(name) }
+  end
 end
