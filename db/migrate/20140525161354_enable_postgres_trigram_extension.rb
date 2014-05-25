c88c59c2494c730d37c056d987299797fb059347
Philip Hale <philip@pghale.com>
2014-05-25 17:19:24 +0100

    Enable Trigram extension for PostgreSQL

---
+++ db/migrate/20140525161354_enable_postgres_trigram_extension.rb
@@ -0,0 +1,8 @@
+class EnablePostgresTrigramExtension < ActiveRecord::Migration
+  def self.up
+    enable_extension 'pg_trgm'
+  end
+  def self.down
+    disable_extension 'pg_trgm'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524143557) do
+ActiveRecord::Schema.define(version: 20140525161354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "addresses", force: true do |t|
     t.string   "line_1"
