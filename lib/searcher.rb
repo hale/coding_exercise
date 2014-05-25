3b9051e9a656e9c12435508fa0483360d6cb74e3
Philip Hale <philip@pghale.com>
2014-05-25 23:15:23 +0100

    Searcher#multi_search generic enough to work with other models.

--- lib/searcher.rb
+++ lib/searcher.rb
@@ -2,7 +2,8 @@ class Searcher
 
   SEARCH_SCOPES = [
     FIRST_NAME = "contact_first_name",
-    LAST_NAME = "contact_last_name"
+    LAST_NAME = "contact_last_name",
+    ADDRESS_LINE_1 = "address_line_1"
   ]
 
   def search(query:, on:)
@@ -13,6 +14,17 @@ class Searcher
   def multi_search(query:, scopes:)
     invalid_scopes = scopes - SEARCH_SCOPES
     raise "Cannot search on #{invalid_scopes.join(" or ")}" if invalid_scopes.any?
-    Contact.search scopes.map{ |scope| scope.gsub(/contact_/, '') }, query
+
+    results = []
+    scopes.each do |scope|
+      model, field = scope.split("_", 2)
+      if model == 'contact'
+        results << (Contact.search field, query)
+      else
+        model_results = model.capitalize.constantize.search field, query
+        results << model_results.map(&:contact)
+      end
+    end
+    results.flatten
   end
 end
--- spec/lib/searcher_spec.rb
+++ spec/lib/searcher_spec.rb
@@ -39,8 +39,18 @@ describe Searcher do
     it "searches on multiple fields" do
       c1 = FactoryGirl.create(:contact, first_name: "Adam")
       c2 = FactoryGirl.create(:contact, last_name: "Jones")
-      results = searcher.multi_search(query: "Adam Jones", scopes: ["contact_first_name", "contact_last_name"])
+      results = searcher.multi_search(
+        query: "Adam Jones",
+        scopes: ["contact_first_name", "contact_last_name"]
+      )
       expect(results).to eq([c1,c2])
     end
+
+    it "errors when provided with an invalid scope" do
+      expect{
+        searcher.multi_search(query: "tst", scopes: ["notascopeever"])
+      }.to raise_error
+    end
+
   end
 end
